00a3de0b74a0fc3a725fd304cf1695668da10470
AndySong <andylauszp@gmail.com>
2019-09-06 11:43:21 +1200

    function js

--- Functions.js
+++ Functions.js
@@ -1,2 +1,41 @@
-const func = (x, y) => x + y;
-console.log(func(1, 2));
+//declare a function
+//this function will be hoisted
+function func(x,y) {
+    return x+y;
+}
+//function expressions
+//Anonymous
+const a= function(x,y){
+    return x+y;
+}
+//arrow
+const b= (x,y)=>{
+    return x+y;
+}
+//arrow with implict return
+const b1= (x,y)=> x+y
+//named
+const c= function sum(x,y){
+    return x+y;
+}
+//IIFE
+const d= (function(x,y) {
+    return x+y;
+})(1,2);
+
+//invoke a function
+const e= func(1,2);
+const e1=new func(1,2);
+const e2=func.call(null,1,2);
+const e3=func.apply(null,[1,2]);
+
+const andy= {
+    name: 'andy',
+    age:30
+}
+
+function displayName(text) {
+    return `${text} ${this.name}`;
+}
+
+console.log(displayName.call(andy,'hello'));
