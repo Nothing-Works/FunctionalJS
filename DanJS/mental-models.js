42189a162f5d3828946c751f7bc341027e9c8463
AndySong <andylauszp@gmail.com>
2020-03-16 16:52:36 +1300

    add mental model

---
+++ DanJS/mental-models.js
@@ -0,0 +1,9 @@
+// declare a variable called a and set it to 10.
+let a = 10;
+// declare a variable called b and set it to a. so b is 10.
+let b = a;
+// set a to 0.
+a = 0;
+//so a is 0, b is 10.
+console.log(a); //0;
+console.log(b); //10;
